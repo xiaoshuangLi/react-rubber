b26b82b0d9989e80bccc723c86cc56f87d5fbe49
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-11-19 20:50:04 +0800

    update react and react-router 3.0

--- frontend/js/components/Loading/Loading.jsx
+++ frontend/js/components/Loading/Loading.jsx
@@ -1,18 +1,14 @@
-import React, { Component } from 'react'
-import TransitionGroup from 'react-addons-transition-group'
+import React, { Component } from 'react';
+import { TransitionGroup } from 'react-transition-group';
 
 export default class Res extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const { show, children } = this.props
+    const { show, children } = this.props;
 
     return (
       <TransitionGroup>
         { show ? children : null }
       </TransitionGroup>
-    )
+    );
   }
 }
