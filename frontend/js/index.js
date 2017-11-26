5166375f2e3c20cea809b3bb7d2732209c65f6a9
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-11-27 00:36:04 +0800

    add something in scss

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,12 @@
 import { render } from 'react-dom';
+import promise from 'es6-promise';
 
 import 'css/index.scss';
-import Global from './container/global'
+import Global from './container/global';
+
+promise.polyfill();
 
 render(
   Global,
   document.getElementById('app')
-)
+);
