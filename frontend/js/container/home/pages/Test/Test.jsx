fc26b0c5c263aad7dc6f523e42941725d487dd05
xiaoshuang.li <xiaoshuang.li@cloudinward.com>
2017-12-18 21:55:39 +0800

    make sass better

--- frontend/js/container/home/pages/Test/Test.jsx
+++ frontend/js/container/home/pages/Test/Test.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+import Raven from 'js/components/Raven';
+
+Raven.watch({
+  test: 2,
+  test2: 3,
+});
+
 class Test extends Component {
   test = () => {
     console.log(this);
