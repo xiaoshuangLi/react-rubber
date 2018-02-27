60dd7cdf98e0cdcf204bed6534dac761d73d7a52
xiaoshuang.li <qq1157502759@gmail.com>
2018-02-27 18:35:23 +0800

    make RelativeRouter Better 3.0

--- frontend/js/components/RelativeRouter/RelativeRouter.jsx
+++ frontend/js/components/RelativeRouter/RelativeRouter.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import App from 'js/components/App';
 
@@ -19,13 +19,20 @@ const createChildrenRoute = (basePath = '', routes = []) => {
 
 class RelativeRouter extends Component {
   render() {
-    const { path = '', routes = [], ...others } = this.props;
+    const {
+      path = '',
+      routes = [],
+      location = {},
+      ...others
+    } = this.props;
 
     const items = createChildrenRoute(path, routes);
 
     return (
       <App {...others}>
-        { items }
+        <Switch location={location}>
+          { items }
+        </Switch>
       </App>
     );
   }
