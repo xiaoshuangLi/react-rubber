0b580ee6cc2fb3a2df2101beff63b16ebd7ca420
xiaoshuang.li <qq1157502759@gmail.com>
2018-03-09 16:54:45 +0800

    update postcss

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -32,22 +32,9 @@ var postLoader = {
   options: {
     plugins() {
       return [
-        require('autoprefixer')({
-          browsers: [
-            'last 3 versions',
-            'ie >= 9',
-            'ie_mob >= 10',
-            'ff >= 30',
-            'chrome >= 34',
-            'safari >= 6',
-            'opera >= 12.1',
-            'ios >= 6',
-            'android >= 4.4',
-            'bb >= 10',
-            'and_uc 9.9',
-          ],
-        }),
+        require('postcss-cssnext')(),
         require('postcss-write-svg')(),
+        require('postcss-reporter')(),
       ];
     },
   },
