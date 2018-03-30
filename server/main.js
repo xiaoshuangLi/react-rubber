df163ec0dcec4d1e433beba3d534a55f9dfd773a
xiaoshuang.li <qq1157502759@gmail.com>
2018-03-30 21:36:00 +0800

    add renderToNodeStream for server-render

--- server/main.js
+++ server/main.js
@@ -2,7 +2,7 @@ import http from 'http';
 import express from 'express';
 
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import { renderToString, renderToNodeStream } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
 
@@ -18,7 +18,32 @@ const createStore = (state = {}) => ({
   dispatch: _ => _,
 });
 
-const createTemp = (parameters = {}) => {
+const splitHtmlTemp = (strings, ...keys) => ({
+  strings,
+  keys,
+});
+
+const partHtmlTemp = splitHtmlTemp`
+  <!DOCTYPE html>
+    <html>
+    <head>
+      <meta http-equiv="content-language" content="zh-CN">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+      <meta name="viewport" content="initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0,viewport-fit=cover">
+      <meta name="format-detection" content="telephone=no>
+      <meta name="mobile-web-app-capable" content="yes">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <title>标题</title>
+      ${'string'}
+    </head>
+    <body>
+      <div id="app">${'stream'}</div>
+      ${'string'}
+    </body>
+  </html>
+`;
+
+const getStyleAndJavascript = (parameters = {}) => {
   const chunks = parameters.chunks();
   const { javascript = {}, styles = {} } = chunks;
 
@@ -27,28 +52,44 @@ const createTemp = (parameters = {}) => {
   const javascriptHtml = Object.values(javascript)
     .reduce((a, b) => `${a}<script src="${b}"></script>`, '');
 
+  return {
+    styles: stylesHtml,
+    javascript: javascriptHtml,
+  };
+};
+
+const createTempString = (parameters = {}) => {
+  const obj = getStyleAndJavascript(parameters);
+  const { javascript = '', styles = '' } = obj;
+
   return (comp) => {
     const compHtml = comp ? renderToString(comp) : '';
+    const list = [styles, compHtml, javascript];
 
-    return `
-      <!DOCTYPE html>
-        <html>
-        <head>
-          <meta http-equiv="content-language" content="zh-CN">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-          <meta name="viewport" content="initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0,viewport-fit=cover">
-          <meta name="format-detection" content="telephone=no>
-          <meta name="mobile-web-app-capable" content="yes">
-          <meta name="apple-mobile-web-app-capable" content="yes">
-          <title>标题</title>
-          ${stylesHtml}
-        </head>
-        <body>
-          <div id="app">${compHtml}</div>
-          ${javascriptHtml}
-        </body>
-      </html>
-    `;
+    return partHtmlTemp.strings.reduce((a, b, index) => `${a}${list[index - 1]}${b}`);
+  };
+};
+
+const createStreamString = (parameters = {}) => {
+  const obj = getStyleAndJavascript(parameters);
+
+  const { javascript = '', styles = '' } = obj;
+  const { keys = [], strings = [] } = partHtmlTemp;
+
+  let start = '';
+  let end = '';
+
+  strings.forEach((item = '', index) => {
+    if (index <= keys.indexOf('stream')) {
+      start = `${start}${item}${index === 0 ? styles : ''}`;
+    } else {
+      end = `${end}${index === strings.length - 1 ? javascript : ''}${item}`;
+    }
+  });
+
+  return {
+    start,
+    end,
   };
 };
 
@@ -57,6 +98,27 @@ export default function (parameters = {}) {
   const server = new http.Server(app);
 
   app.use(express.static('./public'));
+
+  /**
+   * renderToString
+   */
+
+  // app.use((req, res) => {
+  //   const store = createStore();
+  //   const comp = (
+  //     <Provider store={store}>
+  //       <StaticRouter location={req.url} context={{}}>
+  //         { routers }
+  //       </StaticRouter>
+  //     </Provider>
+  //   );
+
+  //   res.send(createTempString(parameters)(comp));
+  // });
+
+  /**
+   * renderToNodeStream
+   */
 
   app.use((req, res) => {
     const store = createStore();
@@ -68,7 +130,15 @@ export default function (parameters = {}) {
       </Provider>
     );
 
-    res.send(createTemp(parameters)(comp));
+    const obj = createStreamString(parameters);
+    const stream = renderToNodeStream(comp);
+
+    res.write(obj.start);
+    stream.pipe(res, { end: false });
+    stream.on('end', () => {
+      res.write(obj.end);
+      res.end();
+    });
   });
 
   server.listen(port, (error) => {
